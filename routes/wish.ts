971291b0244653020a5154e999a86bcc47403903
SushanthK07 <sushanthkummitha@gmail.com>
2023-04-19 21:54:45 +0530

    validate req params with custom validation

--- routes/wish.ts
+++ routes/wish.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from "express";
 import * as z from "zod";
 
-const postRequestSchema = z.object({
+const postRequestBodySchema = z.object({
   names: z.array(z.string()),
+});
+
+const postRequestParamsSchema = z.object({
+  by: z.string().refine((value) => !["undefined", "null"].includes(value), {
+    message: "by should be defined",
+  }),
 });
 
 const postResponseSchema = z.object({
@@ -15,17 +21,21 @@ router.get("/", (req: Request, res: Response) => {
   res.send("Hello " + req.query.name);
 });
 
-router.post("/", (req: Request, res: Response) => {
-  let postData;
+router.post("/:by", (req: Request, res: Response) => {
+  let validatedBody;
+  let validatedParams;
+
   try {
-    postData = postRequestSchema.parse(req.body);
+    validatedBody = postRequestBodySchema.parse(req.body);
+    validatedParams = postRequestParamsSchema.parse(req.params);
   } catch (err) {
     console.error(err);
     return res.status(400).send("Bad request");
   }
 
-  const names = postData.names;
-  const message = "Hello " + names.join(", ");
+  const names = validatedBody.names;
+  const by = validatedParams.by;
+  const message = "Hello " + names.join(", ") + " by " + by;
 
   try {
     return res.send(postResponseSchema.parse({ message }));
