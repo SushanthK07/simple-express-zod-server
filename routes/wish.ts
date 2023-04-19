14591509327e655f3c49b46f21d1722f87157c78
SushanthK07 <sushanthkummitha@gmail.com>
2023-04-19 21:13:03 +0530

    added zod: request validation and type inference

--- routes/wish.ts
+++ routes/wish.ts
@@ -1,13 +1,27 @@
 import { Request, Response } from "express";
+import * as z from "zod";
 
-const router = require('express').Router();
-
-router.get('/', (req: Request, res: Response) => {
-    res.send("Hello " + req.query.name);
+const postSchema = z.object({
+  names: z.array(z.string()),
 });
 
-router.post('/', (req: Request, res: Response) => {
-    res.send("Hello " + req.body.names.join(", "));
+const router = require("express").Router();
+
+router.get("/", (req: Request, res: Response) => {
+  res.send("Hello " + req.query.name);
+});
+
+router.post("/", (req: Request, res: Response) => {
+  let postData;
+  try {
+    postData = postSchema.parse(req.body);
+  } catch (err) {
+    console.error(err);
+    return res.status(400).send("Bad request");
+  }
+
+  const names = postData.names;
+  return res.send("Hello " + names.join(", "));
 });
 
 module.exports = router;
